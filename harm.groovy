62826202b530cf39e179a29f500b3d54f3430240
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-11-30 01:26:10 +0100

    Add files via upload

---
+++ harm.groovy
@@ -0,0 +1,16 @@
+harm -revelance ignore true else direct if change .ip /connect ..dat
+    ivr.replace -farm.xp /win7 /win10
+        secound.os harm.os revelance isp.during groovy.methode
+
+        {
+            secound.win osp.com /sec .parser -icpm.12.140.167.002
+
+                ip.sec /uplink meth.hood -eps .during.free
+                
+                motherboard.any os.p -x .osx durade.pc/intel.amd
+
+                    secundery.pillow .mathcad will.wrong eps
+
+        }   boot.parser bio.lock /synaptic.driver -pass.ok!ready
+
+            durade.lock -free.pass -recogn.enemie root.py /dock .enm
